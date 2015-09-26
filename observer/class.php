83335d02f002540f920f36d8f4a3d89e7066a977
Hometgar <m.redon@outlook.fr>
2015-09-26 09:46:09 +0200

    new class observer

--- observer/class.php
+++ observer/class.php
@@ -8,34 +8,47 @@
       }
 
       public function attachPostit($postit,Mur $mur){
-        return $this->_asAttach[]=new Postite($postit,$this->nom,$mur);
+        $this->_asAttach[]=new Postite($postit,$this->nom,$mur);
+        $key=$this->getPostitKey($postit);
+        $this->_asAttach[$key]->mur->attach($this->_asAttach[$key]);
       }
 
       public function detachPostit($postit){
-        $key=array_search($postit,$this->_asAttach,true);
+        $key=$this->getPostitKey($postit);
         $this->_asAttach[$key]->mur->detach($this->_asAttach[$key]);
+      }
+
+      private function getPostitKey($postit){
+        return array_search($postit,$this->_asAttach,true);
       }
   }
 
   class Postite implements \SplObserver{
     private $_info;
     private $_owner;
+    private $_state;
     public $mur;
-    public $state;
 
     public function __construct($info,$owner,$mur){
       $this->_info=$info;
       $this->_owner=$owner;
       $this->mur=$mur;
-      $this->mur->attach($this);
     }
 
     public function getInfos(){
       return $this->_info;
     }
 
+    public function getState(){
+      return $this->_state;
+    }
+
+    public function setState($state){
+      $this->_state=$state;
+    }
+
     public function update(\SplSubject $mur){
-      if($this->state){
+      if($this->_state){
         mail('mikeyredon@hotmail.fr','Ajout de post-it',$this->_owner.' a mis une post-it au mur qui dit:'.$this->getInfos().', sur le mur '.$mur->couleur);
       }else{
         mail('mikeyredon@hotmail.fr','Retr&eacut; de post-it',$this->_owner.' a horriblement arracher le post-it sur lequel &eacute;tait &eacute;crit :'.$this->getInfos().' , du mur qui est '.$mur->couleur
@@ -53,14 +66,14 @@
     }
     public function attach(\SplObserver $postit) {
         $this->_Postit[]=$postit;
-        $postit->state=true;
+        $postit->setState(true);
         $this->notify();
     }
 
     public function detach(\SplObserver $postit) {
       $key = array_search($postit, $this->_Postit, true);
       if (false !== $key) {
-        $postit->state=false;
+        $postit->setState(false);
         $this->notify();
         unset($this->_Postit[$key]);
       }
